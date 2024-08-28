56ccc55d4f76ad57fa76b6b1d2ffeb6c1c4bc05b
keyla <ktacuabe@udc.edu.ar>
2024-08-28 12:04:46 -0300

    Crear view ver_mensajes

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Mensaje
+from .templates import *
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Holaa bienvenido al index.")
+def ver_mensajes(request):
+    mensajes = Mensaje.objects.all()
+    return render(request, 'mensajes.html',{'mensajes': mensajes})
