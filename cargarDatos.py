777971bab39d56b3b3e1c9e754325b9419622fca
keyla <ktacuabe@udc.edu.ar>
2024-08-28 13:17:40 -0300

    Agregar datos de prueba

--- cargarDatos.py
+++ cargarDatos.py
@@ -2,5 +2,11 @@
 
 from mensajes.models import Mensaje
 
-m1 = Mensaje(texto='hola', remitente='dario barassi', destinatario='keyla', fecha_y_hora='2024-08-27')
-m1.save()
+m1 = Mensaje(texto='hola', remitente='dario', destinatario='keyla', fecha_y_hora='2024-08-27')
+m2 = Mensaje(texto='la verdad no se si usar DateTime está bien', remitente='keyla', destinatario='julio', fecha_y_hora='2024-08-28 12:21')
+m3 = Mensaje(texto='quiero cocinar galletitas con chips de chocolate', remitente='keyla', destinatario='valeria', fecha_y_hora='2024-08-28 12:22')
+m4 = Mensaje(texto='no metas a tu abuela muerta que nadie la falto el respeto', remitente='moria', destinatario='silvina', fecha_y_hora='2011-11-09 23:24')
+m5 = Mensaje(texto='ya no se que poneeer', remitente='keyla', destinatario='julio', fecha_y_hora='2024-08-28 12:36')
+mensajes = [m1, m2, m3, m4, m5]
+for m in mensajes:
+    m.save()
--- mensajes/models.py
+++ mensajes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Mensaje(models.Model):
-    texto = models.TextField()
+    texto = models.TextField(default='hola')
     remitente = models.CharField(max_length=25)
     destinatario = models.CharField(max_length=25)
     fecha_y_hora = models.DateTimeField()
