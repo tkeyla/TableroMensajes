676d90231f239152dca168dfd8e98d9bd7ae1b33
keyla <ktacuabe@udc.edu.ar>
2024-08-28 12:03:58 -0300

    Crear modelo mensaje

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# Texto del mensaje: Contenido del mensaje.
-# Remitente: Usuario que envía el mensaje.
-# Destinatario: Usuario que recibe el mensaje.
-#  Fecha y hora de envío: Timestamp del momento en que se envía el
-
 class Mensaje(models.Model):
     texto = models.TextField()
     remitente = models.CharField(max_length=25)
@@ -13,4 +7,4 @@ class Mensaje(models.Model):
     fecha_y_hora = models.DateTimeField()
 
     def __str__(self):
-        return self
+        return self.texto
